c00a37f8156f8d2cdc9af0da7e8185ffb5d6bd2e
hbkim0104 <30275507+hbkimhbkim@users.noreply.github.com>
2017-10-13 21:03:26 +0900

    Update Fianl_hb.R

--- bigcon code/Fianl_hb.R
+++ bigcon code/Fianl_hb.R
@@ -30,11 +30,8 @@ data_set$LINE_STUS <- as.factor(data_set$LINE_STUS)
 
 # ##int-->factor############(1번)
 data_set$TARGET <- as.factor(data_set$TARGET)
-#
-# ##chr-->integer(AGE): 비식별은 0으로
-# data_set[,52] = as.integer(data_set[,52])
-#
-#
+
+
 # ##'미만','이상'LT1Y_PEOD_RATE 변수 integer로 처리(34번)
 data_set$LT1Y_PEOD_RATE<-gsub('미만','',data_set$LT1Y_PEOD_RATE)
 data_set$LT1Y_PEOD_RATE<-gsub('이상','',data_set$LT1Y_PEOD_RATE)
@@ -86,64 +83,34 @@ str(data_set)
 # ########################
 #
 # [6] TOT_LNIF_AMT : 단위 수정
-data_impu$TOT_LNIF_AMT <- data_impu[,6]*1000
+data_set$TOT_LNIF_AMT <- data_set[,6]*1000
 
 # [7] TOT_CLIF_AMT : 단위 수정
-data_impu$TOT_CLIF_AMT <- data_impu[,7]*1000
+data_set$TOT_CLIF_AMT <- data_set[,7]*1000
 
 # [8] BNK_LNIF_AMT : 단위 수정
-data_impu$BNK_LNIF_AMT <- data_impu[,8]*1000
+data_set$BNK_LNIF_AMT <- data_set[,8]*1000
 
 # [9] CPT_LNIF_AMT : 단위 수정
-data_impu$CPT_LNIF_AMT <- data_impu[,9]*1000
+data_set$CPT_LNIF_AMT <- data_set[,9]*1000
 
 # [15] CB_GUIF_AMT : 단위 수정
-data_impu$CB_GUIF_AMT <- data_impu[,15]*1000
+data_set$CB_GUIF_AMT <- data_set[,15]*1000
 
 # [17] CUST_JOB_INCM : 단위 수정
-data_impu$CUST_JOB_INCM <- data_impu[,17]*10000
+data_set$CUST_JOB_INCM <- data_set[,17]*10000
 
 # [18] HSHD_INFR_INCM : 단위 수정
-data_impu$HSHD_INFR_INCM <- data_impu[,18]*10000
+data_set$HSHD_INFR_INCM <- data_set[,18]*10000
 
 # [23] MATE_JOB_INCM : 단위 수정
-data_impu$MATE_JOB_INCM <- data_impu[,23]*10000
+data_set$MATE_JOB_INCM <- data_set[,23]*10000
 
-# ##############################
-# #####결측치 확인 및 입력######
-# ##############################
-#
-# # ##결측치 확인
-anyNA(data_set)
-colSums(is.na(data_set)) #16, 21, 22, 52,56, 66번 변수에서 결측치 확인
+########################
+#####log변환############
+########################
 
-# Impute missing values with missRanger
-data_impu <- missRanger(data_set, pmm.k = 3, num.trees = 100)
-colSums(is.na(data_impu))
-str(data_impu)
-
-################################
-#####여기서 부터 다시 하자 #####
-################################
-rm(list=ls())
-setwd("C:\\Users\\hanbum\\Desktop\\Data\\Bigcontest")
-data_set <- read.csv('data_impute.csv',header = T, stringsAsFactors = T,
-                     na.strings = c('NULL',''))
-data_set<-data_set[,-1]
-data_set$SEX<-as.factor(data_set$SEX)
-data_set$TARGET<-as.factor(data_set$TARGET)
-
-str(data_set)
-
-#####연체 & 비 연체 그룹으로 분할#########
-data_0 <- data_set[data_set$TARGET==0,]
-data_1 <- data_set[data_set$TARGET==1,]
-
-#############################
-######log변환################
-#############################
-
-# # [6] TOT_LNIF_AMT : log 변환
+# [6] TOT_LNIF_AMT : log 변환
 data_set$TOT_LNIF_AMT <- log(data_set$TOT_LNIF_AMT+1)
 # [7] TOT_CLIF_AMT : log 변환
 data_set$TOT_CLIF_AMT <- log(data_set$TOT_CLIF_AMT + 1)
@@ -151,16 +118,40 @@ data_set$TOT_CLIF_AMT <- log(data_set$TOT_CLIF_AMT + 1)
 data_set$BNK_LNIF_AMT <- log(data_set$BNK_LNIF_AMT + 1)
 # [9] CPT_LNIF_AMT : log 변환
 data_set$CPT_LNIF_AMT <- log(data_set$CPT_LNIF_AMT + 1)
+# [12] CPT_LNIF_AMT : scale 변환
+data_set$CRDT_CARD_CNT <- scale(data_set$CRDT_CARD_CNT)
+# [13] CPT_LNIF_AMT : scale 변환
+data_set$CTCD_OCCR_MDIF <- scale(data_set$CTCD_OCCR_MDIF)
 # [15] CB_GUIF_AMT : log 변환
 data_set$CB_GUIF_AMT <- log(data_set$CB_GUIF_AMT + 1)
+# [17] CB_GUIF_AMT : log 변환
+data_set$CUST_JOB_INCM <- log(data_set$CUST_JOB_INCM + 1)
+# [18] CB_GUIF_AMT : log 변환
+data_set$HSHD_INFR_INCM <- log(data_set$HSHD_INFR_INCM + 1)
+# [23] CB_GUIF_AMT : log 변환
+data_set$MATE_JOB_INCM <- log(data_set$MATE_JOB_INCM + 1)
 # [26] TOT_CRLN_AMT : log 변환
 data_set$TOT_CRLN_AMT <- log(data_set$TOT_CRLN_AMT + 1)
 # [27] TOT_REPY_AMT : log 변환
 data_set$TOT_REPY_AMT <- log(data_set$TOT_REPY_AMT + 1)
+# [28] TOT_REPY_AMT : log 변환
+data_set[,28] <- scale(data_set[,28])
+# [29] TOT_REPY_AMT : scale 변환
+data_set[,29] <- scale(data_set[,29])
+# [30] TOT_REPY_AMT : scale 변환
+data_set[,30] <- scale(data_set[,30])
+# [33] TOT_REPY_AMT : scale 변환
+data_set[,33] <- scale(data_set[,33])
+# [34] STLN_REMN_AMT : scale 변환
+data_set$LT1Y_PEOD_RATE <- scale(data_set$LT1Y_PEOD_RATE)
+# [35] STLN_REMN_AMT : scale 변환
+data_set$AVG_STLN_RATE <- scale(data_set$AVG_STLN_RATE)
 # [36] STLN_REMN_AMT
 data_set$STLN_REMN_AMT <- log(data_set$STLN_REMN_AMT + 1)
 # [37] LT1Y_STLN_AMT : log 변환
 data_set$LT1Y_STLN_AMT <- log(data_set$LT1Y_STLN_AMT +1)
+# [38] LT1Y_STLN_AMT : scale 변환
+data_set$LT1Y_SLOD_RATE <- log(data_set$LT1Y_SLOD_RATE +1)
 # [39] GDINS_MON_PREM
 data_set$GDINS_MON_PREM <-log(data_set$GDINS_MON_PREM +1)
 # [40] SVINS_MON_PREM : log 변환
@@ -175,49 +166,76 @@ data_set$MAX_MON_PREM <- log(data_set$MAX_MON_PREM +1)
 data_set$TOT_PREM <- log(data_set$TOT_PREM +1)
 # [45] FMLY_TOT_PREM : log 변환
 data_set$FMLY_TOT_PREM <- log(data_set$FMLY_TOT_PREM +1)
+
 data_set[,49] <- log(data_set[,49] +1)
 data_set[,54] <- log(data_set[,54] +1)
 data_set[,55] <- log(data_set[,55] +1)
 data_set[,57] <- log(data_set[,57] +1)
 data_set[,58] <- log(data_set[,58] +1)
+data_set[,60] <- log(data_set[,60] +1)
 data_set[,63] <- log(data_set[,63] +1)
+data_set[,65] <- log(data_set[,65] +1)
 data_set[,68] <- log(data_set[,68] +1)
 
-#################################################
-#feature engineering#############################
-#################################################
 
-####category reduction(16번,22번,)#############
 
-# #############################################
-# #one hot encodding###########################
-# #############################################
+# ##############################
+# #####결측치 확인 및 입력######
+# ##############################
 
-#TARGET을 numeric으로 다시 바꿔주자
-# data_set$TARGET<-ifelse(data_set$TARGET=='0',0,1)
-# dmy <- dummyVars( ~ ., data = data_set,fullRank = T)
-# data_transformed <- data.frame(predict(dmy, newdata = data_set))
-# str(data_transformed)
+# # ##결측치 확인
+anyNA(data_set)
+colSums(is.na(data_set)) #16, 21, 22, 52,56, 66번 변수에서 결측치 확인
 
-# #변수선택하기
-# data_set_feature<-data_set[,-c()]
-# str(data_set_zero)
+# Impute missing values with missRanger
+data_impu <- missRanger(data_set, pmm.k = 3, num.trees = 100)
+colSums(is.na(data_impu))
+str(data_impu)
 
+################################
+#####여기서 부터 다시 하자 #####
+################################
+rm(list=ls())
+setwd("C:\\Users\\hanbum\\Desktop\\Data\\Bigcontest")
+data_set <- read.csv('data_set2.csv',header = T, stringsAsFactors = T,
+                     na.strings = c('NULL',''))
+data_set<-data_set[,-1]
+data_set$TARGET<-as.factor(data_set$TARGET)
+data_set$SEX<-as.factor(data_set$SEX)
+data_set$MIN_CNTT_DATE<-as.factor(data_set$MIN_CNTT_DATE)
+str(data_set)
+
+#####연체 & 비 연체 그룹으로 분할#########
+data_0 <- data_set[data_set$TARGET==0,]
+data_1 <- data_set[data_set$TARGET==1,]
+
+
+#######상관계수 보자#############
+data_cor<-data_set
+data_cor$TARGET<-as.integer(data_cor$TARGET)
+str(data_cor)
+data_cor<-data_cor[,-c(16,22,25,53,56,59,66,67)]
+cor1<-cor(data_cor)
+
+
+########변수제거###########
+data_set_eli<-data_set[,-c(12,17,18,19,20,23,26,27,28,29,30,31,32,35,36,42,49,50,54,57,58,61,16,22,25,56)]
 ################################
 #데이터 나누기(8:2)#############
 ################################
 set.seed(1)
-trainIndex <- createDataPartition(data_set$TARGET, p = .8, list=F)
-dataTrain <- data_set[trainIndex,]
-dataTest  <- data_set[-trainIndex,]
+trainIndex <- createDataPartition(data_set_eli$TARGET, p = .8, list=F)
+dataTrain <- data_set_eli[trainIndex,]
+dataTest  <- data_set_eli[-trainIndex,]
 str(dataTrain)
 
 #불균형 맞추기
 set.seed(1)
 #Target to target variable again
-dataTrain$TARGET<-as.factor(dataTrain$TARGET)
-smote_train <- SMOTE(TARGET ~ ., dataTrain, perc.over=600, perc.under=300)
+smote_train <- SMOTE(TARGET ~ ., dataTrain, perc.over=100, perc.under=200)
 table(smote_train$TARGET)
+
+str(smote_train)
 
 #caret은 target에 0,1이 들어가있으면 돌아가지 않는다. 바꿔주자
 smote_train$TARGET<- factor(smote_train$TARGET, levels= c("0", "1"), labels=c("no", "yes"))
@@ -225,8 +243,6 @@ dataTest$TARGET<-factor(dataTest$TARGET, levels= c("0", "1"), labels=c("no", "ye
 
 # Set up for parallel procerssing
 set.seed(1)
-#변수선택 
-smote_train <- smote_train[,-c(16,21,22,23,25,59,61,66)]
 
 # Train xgboost
 traincr <- trainControl(method = "repeatedcv",   # 10fold cross validation
